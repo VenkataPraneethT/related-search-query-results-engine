471e7659cd222d564812866a875dd13d840c2b6b
VenkataPraneethT <venkata.tatiraju@aalto.fi>
2020-04-08 01:16:23 +0530

    restructuring code

--- getSearchQueryResults.js
+++ getSearchQueryResults.js
@@ -3,7 +3,16 @@ const hashTablesWithWordsCount = require('./hashTableWithWordsAndCount');
 const summariesDataSet = require('./data.json');
 const queries = ["is your problems" ,"achieve take book"];
 const numberOfResults = 3;
-const hashMapResults = hashTablesWithWordsCount.formHashTableWithWordsCount(summariesDataSet);
-const result = processResults.getNoOfWordsMatch(hashMapResults, queries, numberOfResults);
-console.log(result);
 
+const getRelevantSearchResults = (queries, numberOfResults) => {
+    const hashMapResults = hashTablesWithWordsCount.formHashTableWithWordsCount(summariesDataSet);
+    const result =  processResults.getNoOfWordsMatch(hashMapResults, queries, numberOfResults);
+    return result;
+}
+
+console.log(getRelevantSearchResults(queries, numberOfResults));
+
+
+module.exports = {
+    getRelevantSearchResults
+}
